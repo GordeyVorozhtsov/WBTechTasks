832b27f154b547e5d0d2a368949c9d82f5862cd3
Gordey <vorozhcov.gordey@rwb.ru>
2025-08-07 18:27:23 +0300

    fix: sort algorithm

--- golang_tasks/WB_L1/wb_l1.16.go
+++ golang_tasks/WB_L1/wb_l1.16.go
@@ -4,20 +4,27 @@ import (
 	"fmt"
 )
 
-// первый алгоритм что мне пришел в голову
-func quickSort(a []int) []int {
-	zeroIdx := 0
-	for zeroIdx < len(a) {
-		for idx := 0; idx < len(a)-1; idx++ {
-			zeroIdx++
-			if a[idx] > a[idx+1] {
-				a[idx], a[idx+1] = a[idx+1], a[idx]
-				zeroIdx = 0
-			}
+func quickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	pivotIndex := len(arr) / 2
+	pivotValue := arr[pivotIndex]
+	var left, equal, right []int
+	for _, v := range arr {
+		if v < pivotValue {
+			left = append(left, v)
+		} else if v == pivotValue {
+			equal = append(equal, v)
+		} else {
+			right = append(right, v)
 		}
 	}
-	return a
+	left = quickSort(left)
+	right = quickSort(right)
+	return append(append(left, equal...), right...)
 }
+
 func main() {
 	fmt.Println(quickSort([]int{5, 1, 7, 2, 4, 9, 3, 8, 6, 10}))
 }
